b39b37a333ffef266f86d400610a5abc97bce4fd
HappypsychoX <jerameyg@gmail.com>
2022-10-14 06:12:34 -0400

    Update Program.vb
    Complete rewrite

--- HabitTracker/Program.vb
+++ HabitTracker/Program.vb
@@ -1,55 +1,118 @@
-Imports System
 Imports Microsoft.Data.Sqlite
-Module Program
-    Sub Main(args As String())
-        CreateDatabase()
-        GetUserInput()
-        Exit Sub
-    End Sub
+
+Public Class Database
 
     Private connectionString As String = "Data Source=habit-Tracker.db"
 
-    Private Sub CreateDatabase()
-        'Creating a connection passing the connection string as an argument
-        'This will create the database For you, there's no need to manually create it.
-        'And no need to use File.Create().
+    Public Sub New()
+        InitializeDatabase()
+    End Sub
+
+    Public Sub Create(recordDate As String, quantity As Integer)
+        Using connection As New SqliteConnection(connectionString)
+            Using tableCmd = connection.CreateCommand()
+                connection.Open()
+                tableCmd.CommandText =
+                    "INSERT INTO yourHabit (Date, Quantity)
+                VALUES ($date, $quantity)"
+                tableCmd.Parameters.AddWithValue("$date", recordDate)
+                tableCmd.Parameters.AddWithValue("$quantity", quantity)
+                tableCmd.ExecuteNonQuery()
+            End Using
+        End Using
+    End Sub
+
+    ''' <summary>
+    ''' Reads all records from database
+    ''' </summary>
+    ''' <returns>List of type Habit</returns>
+    Public Function ReadAll() As List(Of Habit)
+        Dim records As List(Of Habit) = New List(Of Habit)
+        Using connection As New SqliteConnection(connectionString)
+            Using tableCmd = connection.CreateCommand()
+                connection.Open()
+                tableCmd.CommandText =
+                    "SELECT * FROM yourHabit"
+                Dim reader As SqliteDataReader = tableCmd.ExecuteReader()
+
+                While (reader.Read())
+                    Dim idReader As Integer = reader.GetInt32(0)
+                    Dim dateReader As String = reader.GetString(1)
+                    Dim quantityReader As Integer = reader.GetInt32(2)
+                    Dim habit As Habit = New Habit(idReader, dateReader, quantityReader)
+                    records.Add(habit)
+                End While
+
+            End Using
+        End Using
+        Return records
+    End Function
+
+    Public Sub Read()
+        Using connection As New SqliteConnection(connectionString)
+            Using tableCmd = connection.CreateCommand()
+                connection.Open()
+                tableCmd.CommandText =
+                    "SELECT * FROM yourHabit"
+                Dim reader As SqliteDataReader = tableCmd.ExecuteReader()
+
+                While (reader.Read())
+                    Dim dateReader As String = reader.GetString(1)
+                    Dim quantityReader As Integer = reader.GetInt32(2)
+                    Console.WriteLine(dateReader & " " & quantityReader)
+                End While
+            End Using
+        End Using
+    End Sub
+
+    Public Sub Update(id As Integer, recordDate As String, quantity As Integer)
         Using connection As New SqliteConnection(connectionString)
             'Creating the command that wwill be sent to the database
             Using tableCmd = connection.CreateCommand()
                 connection.Open()
                 'Declaring the command (in SQL syntax)
                 tableCmd.CommandText =
-                    "CREATE TABLE IF NOT EXISTS yourHabit(
+                        "UPDATE yourHabit
+                        SET Date = $date,
+                            Quantity = $quantity
+                        WHERE Id = $id"
+                tableCmd.Parameters.AddWithValue("$date", recordDate)
+                tableCmd.Parameters.AddWithValue("$quantity", quantity)
+                tableCmd.Parameters.AddWithValue("$id", id)
+                tableCmd.ExecuteNonQuery()
+            End Using
+        End Using
+    End Sub
+
+    Public Sub Delete(id As Integer)
+        Using connection As New SqliteConnection(connectionString)
+            'Creating the command that wwill be sent to the database
+            Using tableCmd = connection.CreateCommand()
+                connection.Open()
+                'Declaring the command (in SQL syntax)
+                tableCmd.CommandText = "DELETE FROM yourHabit WHERE Id = $id"
+                tableCmd.Parameters.AddWithValue("$id", id)
+                tableCmd.ExecuteNonQuery()
+            End Using
+        End Using
+    End Sub
+
+    ''' <summary>
+    ''' Creates a database if none exists. Also creates as table called yourHabit if none exists
+    ''' </summary>
+    Private Sub InitializeDatabase()
+        Using connection As New SqliteConnection(connectionString)
+            'Creating the command that wwill be sent to the database
+            Using tableCmd = connection.CreateCommand()
+                connection.Open()
+                tableCmd.CommandText =
+                        "CREATE TABLE IF NOT EXISTS yourHabit(
                         Id INTEGER PRIMARY KEY AUTOINCREMENT,
                         Date TEXT,
                         Quantity INTEGER
                         )"
-
-                'Executing the command, which isn't a query, it's not asking to return data from the database.
                 tableCmd.ExecuteNonQuery()
             End Using
-            'We don't need to close the connection or the command. The 'using statement' does that for us.
         End Using
     End Sub
-    ' Once we check if the database exists and create it (or not),
-    'we will call the next method, which will handle the user's input. Your next step is to create this method
-    Private Sub GetUserInput()
-        WriteMenu()
-        Select Case Console.ReadLine()
-            Case "0"
-                Exit Sub
-            Case 1
-
-        End Select
-    End Sub
-
-    Private Sub WriteMenu()
-        Console.WriteLine("Main Menu")
-        Console.WriteLine()
-        Console.WriteLine("Type 0 to Close Application")
-        Console.WriteLine("Type 1 to View All Records")
-        Console.WriteLine("Type 2 to Insert Record")
-        Console.WriteLine("Type 3 to Delete Record")
-        Console.WriteLine("Type 4 to Update Record")
-    End Sub
-End Module
+End Class
